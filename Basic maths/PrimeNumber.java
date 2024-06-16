cd5060f8f6fad840595cadf6da9f403d2f8f2838
MitulPatil <patilmitul1911@gmail.com>
2024-06-16 23:03:02 +0530

    prime number

---
+++ Basic maths/PrimeNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    static void PrintDivisors(int N){
+        int count=0;
+        for(int i=1;i<=(int)Math.sqrt(N);i++){
+            if(N%i==0) {
+                count++;
+                if((N/2) != i){
+                    count++;
+                }
+            }
+        }
+        if(count==2) System.out.println("prime");
+        else System.out.println("not prime");
+    }
+    public static void main(String[] args) {
+        System.out.print("enter number");
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        PrintDivisors(N);
+        sc.close();
+    }
+}
